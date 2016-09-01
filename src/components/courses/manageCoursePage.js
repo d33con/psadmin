18c5374d0a9d1c7bd3d24db2d17a12cab413cd54
Oli Bullen <d33con@gmail.com>
2016-09-01 14:46:22 +0900

    update link regex check

--- src/components/courses/manageCoursePage.js
+++ src/components/courses/manageCoursePage.js
@@ -64,7 +64,7 @@ var ManageCoursePage = React.createClass({
       formIsValid = false;
     }
 
-    var re = /((https?|ftp):\/\/|www\.)[^\s/$.?#].[^\s]*/g;
+    var re = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
     if (!re.test(this.state.course.watchHref)) {
       this.state.errors.watchHref = 'Please enter a valid URL';
       formIsValid = false;
